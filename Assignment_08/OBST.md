35c261838fb82d67dc4342d112ce1168530a4180
Amey Kulkarni <148298416+ameyk2004@users.noreply.github.com>
2024-05-10 14:46:21 +0530

    OBST

---
+++ Assignment_08/OBST.md
@@ -0,0 +1,16 @@
+# Optimal Binary Search Tree (OBST)
+
+## Binary Search Tree
+<br>
+<pre>
+    The maximum searches required to search in BST is the height of BST
+</pre>
+<br>
+<img src="https://miro.medium.com/v2/resize:fit:1400/1*tUBYCHi32Zj0B2UCw0qmlA.png" width="100%">
+
+In above BST the height is 4, therefore to serch an element present in tree (successful search) or to search an element not present in the tree (unsuccessful search) max comparisons is 4.
+
+<hr>
+<h2>Possible Binary Search Trees</h2>
+
+<img src="https://i.imgur.com/PWpiG7Q.png" width="100%">
---
+++ Assignment_08/assignment8.cpp
@@ -0,0 +1,183 @@
+#include<iostream>
+#include<limits.h>
+
+using namespace std;
+
+class Node{
+    int k;
+    int p;
+    int q;
+    Node* left;
+    Node* right;
+public:
+    Node() {}
+    Node(int k, int p, int q) : k(k), p(p), q(q), left(nullptr), right(nullptr){} //Node created
+    friend class OBST;
+};
+
+class MatrixElement{
+    int weight;
+    int cost;
+    int root;
+
+    public:
+    MatrixElement() {}
+    MatrixElement(int w, int c, int r) : weight(w), cost(c), root(r) {}
+
+    friend class OBST;
+};
+
+class OBST{
+    MatrixElement** matrix;
+    int nodes;
+    Node* root;
+    Node* nodeList;
+
+    public:
+    OBST(int n=0) : nodes(n+1)
+    {
+        root = nullptr;
+        matrix = new MatrixElement*[nodes];
+        nodeList = new Node[nodes];
+
+        for(int i=0;i<nodes;i++)
+        {
+            matrix[i] = new MatrixElement[nodes];
+        }
+    }
+
+    void instantiateNodeList()
+    {
+        cout<<"Ener values of k, p and q"<<endl;
+        int k=-1;
+        int p=-1;
+        int q=-1;
+        for(int i = 0;i<nodes;i++)
+        {
+            if(i!=0)
+            {
+                cout<<"Enter k"<<i<<" : ";
+                cin>>k;
+                cout<<"Enter p"<<i<<" : ";
+                cin>>p;
+            }
+            cout<<"Enter q"<<i<<" : ";
+            cin>>q;
+
+            nodeList[i] = Node(k,p,q);
+        }
+    }
+
+    void instantiateMatrix()
+    {
+        for(int i=0;i<nodes;i++)
+        {
+            for(int j=0;j<nodes;j++)
+            {
+                if(i==j)
+                {
+                    // cout<<i<<" "<<j<<endl;
+                    matrix[i][j] = MatrixElement(nodeList[i].q,0,-1);
+                }
+                else
+                {
+                  matrix[i][j] = MatrixElement(-1,-1,-1);
+                }
+                
+            }
+        }
+
+    }
+
+    void optimise()
+    {
+        for(int diff=0;diff<nodes;diff++)
+        {
+            for(int i=0;i<nodes;i++)
+            {
+                for(int j=0;j<nodes;j++)
+                {
+                    if(diff == j-i)
+                    {
+                        C(i,j);
+                        // cout<<"we for "<<i<<" "<<j<<endl;
+                        // printMatrix();
+                    //  cout<<"hello diff "<<diff<<endl;
+                    }
+                }
+            }
+        }
+    }
+
+    int C(int i, int j)
+    {
+        if(i>=j) return 0;
+        if(matrix[i][j].cost != -1) return matrix[i][j].cost;
+
+        int minCost = INT_MAX;
+        int minRoot = -1;
+
+        for(int k=i+1;k<=j;k++)
+        {
+            int cost = C(i,k-1) + C(k,j) + W(i,j);
+
+            if(cost < minCost)
+            {
+                minCost = cost;
+                minRoot = k;
+            }
+        }
+
+        matrix[i][j].cost = minCost;
+        matrix[i][j].root = minRoot;
+        return minCost;
+    }
+
+    int W(int i, int j)
+    {
+        if(i>j) return 0;
+        if(i==j) return matrix[i][j].weight;
+
+        if(matrix[i][j].weight!=-1) return matrix[i][j].weight;
+
+        int weight = W(i,j-1) + nodeList[j].p + nodeList[j].q;
+
+        matrix[i][j].weight = weight;
+        // cout<<"Weight for "<<i<<" "<<j<<" = "<<weight<<endl;
+        return weight;
+    }
+
+    void dispNodeList()
+    {
+        for(int i=0;i<nodes;i++)
+        {
+            cout<<nodeList[i].k<<" "<<nodeList[i].p<<" "<<nodeList[i].q<<endl;
+        }
+    }
+
+    void printMatrix()
+    {
+        cout<<"\n\n";
+        for(int i=0;i<nodes;i++)
+        {
+            for(int j=0;j<nodes;j++)
+            {
+                cout<<matrix[i][j].cost<<" ";
+            }
+
+            cout<<endl;
+        }
+    }
+
+};
+
+int main()
+{
+    OBST tree(4);
+    tree.instantiateNodeList();
+    tree.instantiateMatrix();
+    tree.printMatrix();
+    // tree.dispNodeList();
+    tree.optimise();
+    tree.printMatrix();
+}
