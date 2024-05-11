4b0f729a62caaede377c0729c54a16e69c5cf185
Amey Kulkarni <148298416+ameyk2004@users.noreply.github.com>
2024-05-11 08:18:36 +0530

    Assn 10

--- Assignment_10/assignment10.cpp
+++ Assignment_10/assignment10.cpp
@@ -1,87 +1,72 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
 class Heap{
-    int size;
+private:
     int* arr;
+    int size;
 
-    public:
-    Heap(int s, int* arr)
-    {
-        size = s +1;
-        this->arr = new int[s+1]; 
-         this->arr[0] = -1;
-        for(int i=0;i<size;i++)
-        {
+    void heapify(int i, int n){
+        int largest = i;
+        int leftChild = 2*i;
+        int rightChild = 2*i + 1;
+
+        if(leftChild <= n && arr[leftChild] > arr[largest]){
+            largest = leftChild;
+        }
+
+        if(rightChild <= n && arr[rightChild] > arr[largest]){
+            largest = rightChild;
+        }
+
+        if(i != largest){
+            swap(arr[largest],arr[i]);
+            heapify(largest, n);
+        }
+
+    }
+
+public:
+    Heap(int* arr, int size){
+        this->size = size+1;
+        this->arr = new int[size+1];
+        this->arr[0] = -1;
+        for(int i=0; i<size; i++){
             this->arr[i+1] = arr[i];
         }
     }
 
-    void heapify(int i, int n)
-    {
-        int largest = i;
-        int left = 2*i;
-        int right = 2*i+1;
-
-        if(arr[largest] < arr[left])
-            largest = left;
-
-        if(arr[largest] < arr[right])
-            largest = right;
-
-        if(largest !=i)
-        {
-            swap(arr[largest], arr[i]);
-            heapify(largest, n);
-        }
-        
-    }
-
-    void make_heap()
-    {
-        int n = size -1;
-        for(int i=n/2;i>0;i--)
-        {
+    void make_heap(){
+        int n = size-1;
+        for(int i=n/2; i>0; i-- ){
             heapify(i,n);
         }
     }
 
-    void heapSort()
-    {
-        int n=size-1;
-        while(n>1)
-        {
-           swap(this->arr[1], this->arr[n]);
-           n--;
-           heapify(1,n);
-        }
-
-
-    }
-
-    void print()
-    {
-        cout<<"\n";
-        for(int i=1;i<size;i++)
-        {
-            cout<<this->arr[i]<<" ";
+    void heapSort(){
+        int n = size-1;
+        while(n>1){
+            swap(arr[1],arr[n]);
+            n--;
+            heapify(1,n);
         }
     }
 
-    //5
-    //4 1 
+    void print(){
+        for(int i=1; i<size; i++){
+            cout<<arr[i]<<" ";
+        }
+        cout<<endl;
+    }
 };
 
-
-int main()
-{
+int main(){
     int arr[] = {4,2,1,5,3};
-    Heap heap(5,arr);
-    heap.print();
-    heap.make_heap();
-    heap.print();
-    heap.heapSort();
-    heap.print();
-    
-
-}
+    int size = 5;
+    Heap h(arr,size);
+    h.print(); 
+    h.make_heap();
+    h.print();
+    h.heapSort();
+    h.print();
+}   
